605767734a54055e36f22e55e413b4db2f91425f
NicollyMazeika <nicollydepaula10@gmail.com>
2026-04-22 13:44:42 -0300

    feat: atualiza subração

--- Calculadoraa/src/operacoes/Subtração.java
+++ Calculadoraa/src/operacoes/Subtração.java
@@ -4,10 +4,18 @@
  */
 package operacoes;
 
+import java.util.Scanner;
+
 /**
  *
  * @author info226
  */
 public class Subtração {
+       public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x-y);
+    }
     
 }
